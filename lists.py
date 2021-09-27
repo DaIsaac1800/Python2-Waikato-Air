7c6c66a8796aabe655aaf45343e9672d4ba2fd02
Bob <86213217+DaIsaac1800@users.noreply.github.com>
2021-09-27 02:01:54 +0000

    I added a dictionary for prices and made the list system for the destinations actually worked.

--- lists.py
+++ lists.py
@@ -1,1 +1,4 @@
 destinations = ("Waikato to Wellington", "Wellington to Waikato")
+
+prices = {"1": 109.00,
+"2": 109.00}
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import lists
-# imports the list file to be used
 import functions
+# imports the list file to be used
 # imports all the functions file to be used
 
 print("Welcome to waikato air booking services")
 
 functions.linebreak()
-# is line break
+# is line break repeated multiple times
 
 name = input("What would you like to be called? ").strip().lower()
 # asks for users name and saves as name
 functions.linebreak()
 
-for x in lists.destinations[]:
-    print x
+print("{} please choose a destination (Using numbers)".format(name.capitalize()))
+# used to help user understand what to do
+print()
+
+y = 0
+for x in lists.destinations:
+    y += 1
+    print("{}: {}".format(y, x))
+# used to print out the destinations
+while True:
+    choice = input("Answer here: ")
+    if choice in lists.prices:
+        break
+    else:
+        print("That is not a destination")
