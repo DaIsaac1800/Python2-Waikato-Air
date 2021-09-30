3b5f63ecee723893af2831dc28d50d271bb00f9f
Bob <86213217+DaIsaac1800@users.noreply.github.com>
2021-09-30 22:35:57 +0000

    Made The list a function. Makes it easy to do something in the future if I want for some reason

--- functions.py
+++ functions.py
@@ -2,3 +2,8 @@ def linebreak():
     print('''
 --------------------------------------------''')
 
+def list(dict, sub_list):
+    y = 1
+    for x in dict:
+        print("{}: {}".format(y, dict[x][sub_list]))
+        y += 1 
--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import functions
 
 print("Welcome to waikato air booking services")
 
-print(lists.destinations)
-
 functions.linebreak()
 # is line break repeated multiple times
 
@@ -14,18 +12,15 @@ name = input("What would you like to be called? ").strip().lower()
 # asks for users name and saves as name
 functions.linebreak()
 
-print("{} please choose a destination (Using numbers)"
+print("{}, please choose a destination (Using numbers)"
 .format(name.capitalize()))
 # used to help the user understand what to do
 print()
 
-y = 1
-for x in lists.destinations:
-    print("{}: {}".format(y, lists.destinations[x]['destination']))
-    y += 1
+functions.list(lists.destinations, 'destination')
 # used to print out the destinations
 while True:
-    choice = int(input("Answer here: "))
+    choice = input("Answer here: ")
     if choice in lists.destinations:
         break
     else:
