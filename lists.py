7e04feb1bb3be4655e648aad5770b19f4b886e75
Bob <86213217+DaIsaac1800@users.noreply.github.com>
2021-11-08 01:17:18 +0000

    Changed the prices of the destinations.

--- lists.py
+++ lists.py
@@ -1,11 +1,11 @@
 # this file is used to store all the list and dictionaries
 
 # this dictonariies is used to all destinations avaliable
-destinations = {'1': {'destination': 'Waikato to Wellington', 'price': 109.00},
-                '2': {'destination': 'Wellington to Waikato', 'price': 109.00},
+destinations = {'1': {'destination': 'Waikato to Wellington', 'price': 38.00},
+                '2': {'destination': 'Wellington to Waikato', 'price': 38.00},
                 '3': {'destination': 'Waikato to Chirstchurch',
-                      'price': 109.00},
+                      'price': 41.00},
                 '4': {'destination': 'Chirstchurch to Waikato',
-                      'price': 109.00}}
+                      'price': 41.00}}
 
 confirmation = (('yes', '1'), ('no', '2'))
