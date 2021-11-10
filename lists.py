2ef1aecf9a0b5cb60cb0ec56ca79e250b7cdf854
Bob <86213217+DaIsaac1800@users.noreply.github.com>
2021-11-10 20:17:26 +0000

    Added 'Waikato to Queenstown' and it's opposite as a new Destination to my list, bringing the total up to 6

--- lists.py
+++ lists.py
@@ -6,7 +6,9 @@ destinations = {'1': {'destination': 'Waikato to Wellington', 'price': 38.00},
                 '3': {'destination': 'Waikato to Chirstchurch',
                       'price': 41.00},
                 '4': {'destination': 'Chirstchurch to Waikato',
-                      'price': 41.00}}
+                      'price': 41.00},
+                '5': {'destination': 'Waikato to Queenstown', 'price': 69.00},
+                '6': {'destination': 'Queenstown to Waikato', 'price': 69.00}}
 
 # just a simple list so i can use words too
 confirmation = (('yes', '1'), ('no', '2'))
