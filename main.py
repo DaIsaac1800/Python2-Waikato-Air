c9418cd1f582c4985f49db5a31f5107e5064b613
Bob <86213217+DaIsaac1800@users.noreply.github.com>
2021-11-22 23:25:15 +0000

    Added a while True loop fixed an issue with the cancelation of the loop and made the loop look pretty when running.

--- main.py
+++ main.py
@@ -3,30 +3,38 @@ from myfunctions import *
 # imports the list file to be used
 # imports the functions file to be used
 
-print("Welcome to waikato air booking services")
+while True:
+# makes it so the program repeats
+    print("Welcome to waikato air booking services")
 
-linebreak()
+    linebreak()
 # is line break repeated multiple times
 
-name_string = name()
+    name_string = name()
 # asks for users name and saves as name
 
-choice = destination_calculate(destinations, 'destination',
+    choice = destination_calculate(destinations, 'destination',
                                name_string,
-                               'Is this the correct destination',
+                               'Is this the correct destination?',
                                confirmation_list)
 # used to print out the destinations
 # this is for choosing a destination from the list
 # prints the price and destination of the flight
 # calls the confirmer to ask if correct destination returns choice
 
-
-confirm = confirmer('Are you flying tomorrow',
-                    confirmation_list)
-linebreak()
-if confirm in confirmation_list[0]:
-    calculate(destinations, choice)
+    confirm = confirmer('Are you flying tomorrow?',
+                        confirmation_list)
     linebreak()
+    if confirm in confirmation_list[0]:
+        calculate(destinations, choice)
+        linebreak()
 # used to ask and calculate a discount avalaible to the user
+    
+    confirm = confirmer('Do you want to book agian?',
+                        confirmation_list)
+    linebreak()
+    if confirm in confirmation_list[1]:
+        break
+# confirmer and If statement thats makes the while true loop continue
 
 print('Thank you for booking with Waikato air')
